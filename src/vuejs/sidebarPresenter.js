7ef1c18eba4ada31e618055a626a9371c1bdee1b
Alexander Widman <widma@kth.se>
2022-11-05 17:10:50 +0100

    1.4 done

--- src/vuejs/sidebarPresenter.js
+++ src/vuejs/sidebarPresenter.js
@@ -2,5 +2,7 @@ import SidebarView from "../views/sidebarView.js";
 
 export default
 function Sidebar(props){
-    return <SidebarView number={props.model.numberOfGuests}/>;
+    function onNumberChangeACB(value){ props.model.setNumberOfGuests(value)}
+    return <SidebarView number={props.model.numberOfGuests}
+                        onNumberChange={onNumberChangeACB}/>;
 }
