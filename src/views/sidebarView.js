b48530684d56612fd5db9abce73f8c04c4b973bd
Alexander Widman <widma@kth.se>
2022-11-06 01:42:04 +0100

    1.5 progress

--- src/views/sidebarView.js
+++ src/views/sidebarView.js
@@ -1,3 +1,5 @@
+import { menuPrice, dishType, sortDishes } from "../utilities";
+
 function SidebarView(props){
     function minusButtonClickACB(){ props.onNumberChange(props.number-1)}
     function plusButtonClickACB(){ props.onNumberChange(props.number+1)}
@@ -12,8 +14,40 @@ function SidebarView(props){
             enabled={props.number}
             onClick={plusButtonClickACB}
         >+</button>
+        {
+            renderDishes(props.dishes, props.number)
+        }
     </div>
     );
 }
 
+function renderDishes(dishArray, guests){
+    function dishTableRowCB(dish){
+        return <tr key={dish.id}>
+            <button>x</button>
+            <td>{dish.title}</td>
+            <td class="center">{dishType(dish)}</td>
+            <td class="right">{(dish.pricePerServing * guests).toFixed(2)}</td></tr>;
+    }
+
+    return <table>
+        <tbody>
+        {
+            sortDishes(dishArray).map(dishTableRowCB)
+        }
+        <tr>
+            <td></td>
+            <td>Total:</td>
+            <td></td>
+            <td>
+                {
+                    (menuPrice(dishArray) * guests).toFixed(2)
+                }
+            </td>
+        </tr>
+        </tbody>
+        </table>;
+}
+
 export default SidebarView;
+export {renderDishes};   // we export so that tests can analyze the source code
--- src/vuejs/sidebarPresenter.js
+++ src/vuejs/sidebarPresenter.js
@@ -4,5 +4,6 @@ export default
 function Sidebar(props){
     function onNumberChangeACB(value){ props.model.setNumberOfGuests(value)}
     return <SidebarView number={props.model.numberOfGuests}
+                        dishes={props.model.dishes}
                         onNumberChange={onNumberChangeACB}/>;
 }
